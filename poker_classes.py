4ed6e9a9033f59da9e6cb53feb6064793a8988ad
Yiply42 <willlash42@yahoo.com>
2025-02-10 17:29:12 -0500

    Created Video Poker Game

---
+++ poker_classes.py
@@ -0,0 +1,169 @@
+from enum import Enum
+import random
+
+class Suit(Enum):
+    HEART = ("H", 0)
+    DIAMOND = ("D", 1)
+    CLUB = ("C", 2)
+    SPADE = ("S", 3)
+
+    def __str__(self):
+        return self.value[0]
+
+class Rank(Enum):
+    TWO = ("2", 0)
+    THREE = ("3", 1)
+    FOUR = ("4", 2)
+    FIVE = ("5", 3)
+    SIX = ("6", 4)
+    SEVEN = ("7", 5)
+    EIGHT = ("8", 6)
+    NINE = ("9", 7)
+    TEN = ("T", 8)
+    JACK = ("J", 9)
+    QUEEN = ("Q", 10)
+    KING = ("K", 11)
+    ACE = ("A", 12)
+
+    def __str__(self):
+        return self.value[0]
+    
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card(rank, suit) for suit in Suit for rank in Rank]
+        self.shuffle()
+
+    def __str__(self):
+        str = ""
+        for card in self.cards:
+            str += f"{card}, "
+        return str.strip(", ")
+    
+    def __len__(self):
+        return len(self.cards)
+    
+    def draw(self):
+        return self.cards.pop()
+    
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        return f"{self.rank}{self.suit}"
+
+    def __eq__(self, other):
+        return self.rank == other.rank and self.suit == other.suit
+
+    def __lt__(self, other):
+        return self.rank.value[1] < other.rank.value[1]
+
+    def __gt__(self, other):
+        return self.rank.value[1] > other.rank.value[1]
+
+    def __le__(self, other):
+        return self.rank.value[1] <= other.rank.value[1]
+
+    def __ge__(self, other):
+        return self.rank.value[1] >= other.rank.value[1]
+
+    def __ne__(self, other):
+        return self.rank != other.rank or self.suit != other.suit
+    
+class Hand:
+    def __init__(self, cards):
+        self.cards = cards
+        self.suit_counts = [0] * 4
+        self.rank_counts = [0] * 13
+        for card in cards:
+            self.suit_counts[card.suit.value[1]] += 1
+            self.rank_counts[card.rank.value[1]] += 1
+        self.sort()
+
+    def __str__(self):
+        str = ""
+        for card in self.cards:
+            str += f"{card}, "
+        return str.strip(", ")
+    
+    def replace_card(self, index_or_card, new_card):
+        if isinstance(index_or_card, int):
+            self.remove_card(index_or_card)
+            self.add_card(new_card, index_or_card)
+        else:
+            for i in range(len(self.cards)):
+                if self.cards[i] == index_or_card:
+                    self.remove_card(i)
+                    self.add_card(new_card, i)
+                    break
+
+    def remove_card(self, index):
+        self.rank_counts[self.cards[index].rank.value[1]] -= 1
+        self.suit_counts[self.cards[index].suit.value[1]] -= 1
+        self.cards[index] = None
+
+    def add_card(self, card, index):
+        self.cards[index] = card
+        self.rank_counts[card.rank.value[1]] += 1
+        self.suit_counts[card.suit.value[1]] += 1
+
+    def sort(self):
+        self.cards.sort(reverse = True)
+
+    def get_hand(self):
+        """
+            returns (hand, hand_value)
+        """
+        if self.is_royal_flush():
+            return ("Royal Flush", 8000)
+        if self.is_straight_flush():
+            return ("Straight Flush", 500)
+        if self.is_four_of_a_kind():
+            return ("4-of-a-Kind", 250)
+        if self.is_full_house():
+            return ("Full House", 90)
+        if self.is_flush():
+            return ("Flush", 60)
+        if self.is_straight():
+            return ("Straight", 40)
+        if self.is_three_of_a_kind():
+            return ("3-of-a-Kind", 30)
+        if self.is_two_pair():
+            return ("Two Pair", 20)
+        if self.is_high_pair():
+            return ("High Pair", 10)
+        if self.is_pair():
+            return ("Low Pair", 5)
+        return ("Junk", 0)
+    
+    def is_royal_flush(self):
+        return self.is_straight_flush() and self.cards[0].rank == Rank.TEN
+    def is_straight_flush(self):
+        return self.is_flush() and self.is_straight()
+    def is_four_of_a_kind(self):
+        return 4 in self.rank_counts
+    def is_full_house(self):
+        return 3 in self.rank_counts and 2 in self.rank_counts
+    def is_flush(self):
+        return 5 in self.suit_counts
+    def is_straight(self):
+        for i in range(4):
+            if self.cards[i].rank.value[1] != self.cards[i - 1].rank.value[1] + 1:
+                return False
+        return True
+    def is_three_of_a_kind(self):
+        return 3 in self.rank_counts
+    def is_two_pair(self):
+        return self.rank_counts.count(2) == 2
+    def is_high_pair(self):
+        for i in range(Rank.TEN.value[1], Rank.ACE.value[1] + 1):
+            if self.rank_counts[i] > 1:
+                return True
+        return False
+    def is_pair(self):
+        return 2 in self.rank_counts
---
+++ video_poker.py
@@ -0,0 +1,83 @@
+from poker_classes import Deck, Hand
+from enum import Enum
+import torch
+
+class Action(Enum):
+    HOLD = 0
+    DISCARD = 1
+
+class VideoPokerGame:
+    def __init__(self):
+        self.deck = None
+        self.hand = None 
+        self.hand_history = []
+        self.turn_number = 1
+        self.state = None 
+        
+    def play(self, is_human = True, agent = None):
+        if not is_human and agent is None:
+            raise ValueError("You must provide an agent if you are not playing as a human")
+        playing = True
+        while playing:
+            self.deck = Deck()
+            self.hand = Hand([self.deck.draw() for _ in range(5)])
+            self.hand_history = []
+            self.turn_number = 1
+            self.state = State(self.hand, self.deck, self.turn_number)
+            self.hand_history.append(self.state)
+
+            while self.turn_number < 4:
+                if is_human:
+                    print(f"Turn {self.turn_number}/3\n Your Hand: {self.hand} | {self.hand.get_hand()[0]}")
+
+                    valid_input = False
+                    while not valid_input:
+                        ans = input("Select the cards you would like to hold (1-5). Example: \'135\'\t")
+                        if len(ans) > 5 or len(ans.strip("12345 ")) > 0:
+                            print("Invalid Input. Select the cards you would like to hold (1-5). Example: To hold the 1st, 3rd, and 5th cards, type \'135\'")
+                        else:
+                            valid_input = True
+                    actions = torch.tensor([0 if str(i + 1) in ans else 1 for i in range(5)])
+                else:
+                    actions = agent.get_action(self.state)
+                
+                self.take_turn(actions)
+                self.state = State(self.hand, self.deck, self.turn_number)
+                self.hand_history.append(self.state)
+            hand_type, hand_value = self.hand.get_hand()
+            if is_human:
+                print(f"Final Hand: {self.hand}")
+                print(f"Result: {hand_type}, Payout: {hand_value}")
+                response = input("Play Again? (y/n)").lower().strip()
+                if response == 'no' or response == 'n':
+                    print("Goodbye!")
+                    playing = False
+            else:
+                agent.give_reward(hand_value)
+                playing = False
+            
+    def take_turn(self, actions):
+        if len(actions) != 5:
+            raise ValueError("You must select 5 cards to hold")
+        if self.turn_number >= 4:
+            raise ValueError("The game is over, no more actions can be taken")
+        
+        self.turn_number += 1
+
+        for i, action in enumerate(actions):
+            print(action.item())
+            if action == 1:
+                self.hand.replace_card(i, self.deck.draw())
+        self.hand.sort()
+        
+
+class State:
+    def __init__(self, hand, deck, turn):
+        self.hand = hand
+        self.deck = deck
+        self.turn = turn
+    
+
+if __name__ == "__main__":
+    VideoPokerGame().play()
+    
